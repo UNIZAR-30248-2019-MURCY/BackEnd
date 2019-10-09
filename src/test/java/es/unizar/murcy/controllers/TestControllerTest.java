8f38756da46c65432deed756451bb9a44986db01
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-10-09 12:20:47 +0200

    enable minum code coverage

---
+++ src/test/java/es/unizar/murcy/controllers/TestControllerTest.java
@@ -0,0 +1,12 @@
+package es.unizar.murcy.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestControllerTest {
+
+    @Test
+    public void helloWorld() {
+    }
+}
